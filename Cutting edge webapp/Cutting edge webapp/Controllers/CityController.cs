6916c45fe0bc600d326306c81b82e5d516a28890
Landeeyo <landeeyo@10g.pl>
2014-03-15 19:16:05 +0100

    Added auto-generated controllers and view for entity management.

---
+++ Cutting edge webapp/Cutting edge webapp/Controllers/CityController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Cutting_edge_webapp.Models;
+using Cutting_edge_webapp.DAL;
+
+namespace Cutting_edge_webapp.Controllers
+{
+    public class CityController : Controller
+    {
+        private LocalizationContext db = new LocalizationContext();
+
+        //
+        // GET: /City/
+
+        public ActionResult Index()
+        {
+            var cities = db.Cities.Include(c => c.Country);
+            return View(cities.ToList());
+        }
+
+        //
+        // GET: /City/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            City city = db.Cities.Find(id);
+            if (city == null)
+            {
+                return HttpNotFound();
+            }
+            return View(city);
+        }
+
+        //
+        // GET: /City/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.CountryID = new SelectList(db.Countries, "CountryID", "Name");
+            return View();
+        }
+
+        //
+        // POST: /City/Create
+
+        [HttpPost]
+        public ActionResult Create(City city)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cities.Add(city);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CountryID = new SelectList(db.Countries, "CountryID", "Name", city.CountryID);
+            return View(city);
+        }
+
+        //
+        // GET: /City/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            City city = db.Cities.Find(id);
+            if (city == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CountryID = new SelectList(db.Countries, "CountryID", "Name", city.CountryID);
+            return View(city);
+        }
+
+        //
+        // POST: /City/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(City city)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(city).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CountryID = new SelectList(db.Countries, "CountryID", "Name", city.CountryID);
+            return View(city);
+        }
+
+        //
+        // GET: /City/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            City city = db.Cities.Find(id);
+            if (city == null)
+            {
+                return HttpNotFound();
+            }
+            return View(city);
+        }
+
+        //
+        // POST: /City/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            City city = db.Cities.Find(id);
+            db.Cities.Remove(city);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Cutting edge webapp/Cutting edge webapp/Controllers/StreetController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Cutting_edge_webapp.Models;
+using Cutting_edge_webapp.DAL;
+
+namespace Cutting_edge_webapp.Controllers
+{
+    public class StreetController : Controller
+    {
+        private LocalizationContext db = new LocalizationContext();
+
+        //
+        // GET: /Street/
+
+        public ActionResult Index()
+        {
+            var streets = db.Streets.Include(s => s.City);
+            return View(streets.ToList());
+        }
+
+        //
+        // GET: /Street/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Street street = db.Streets.Find(id);
+            if (street == null)
+            {
+                return HttpNotFound();
+            }
+            return View(street);
+        }
+
+        //
+        // GET: /Street/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.CityID = new SelectList(db.Cities, "CityID", "Name");
+            return View();
+        }
+
+        //
+        // POST: /Street/Create
+
+        [HttpPost]
+        public ActionResult Create(Street street)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Streets.Add(street);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CityID = new SelectList(db.Cities, "CityID", "Name", street.CityID);
+            return View(street);
+        }
+
+        //
+        // GET: /Street/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Street street = db.Streets.Find(id);
+            if (street == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CityID = new SelectList(db.Cities, "CityID", "Name", street.CityID);
+            return View(street);
+        }
+
+        //
+        // POST: /Street/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Street street)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(street).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CityID = new SelectList(db.Cities, "CityID", "Name", street.CityID);
+            return View(street);
+        }
+
+        //
+        // GET: /Street/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Street street = db.Streets.Find(id);
+            if (street == null)
+            {
+                return HttpNotFound();
+            }
+            return View(street);
+        }
+
+        //
+        // POST: /Street/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Street street = db.Streets.Find(id);
+            db.Streets.Remove(street);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
