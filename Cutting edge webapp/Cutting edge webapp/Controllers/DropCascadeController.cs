b04af174acf7f98394225a97c337b412af8dcfd8
Landeeyo <landeeyo@10g.pl>
2014-03-16 20:54:27 +0100

    Implemented passing data from view to model (at POST). Placed DropDownListFor instead of DropDownList and HTML selects.

--- Cutting edge webapp/Cutting edge webapp/Controllers/DropCascadeController.cs
+++ Cutting edge webapp/Cutting edge webapp/Controllers/DropCascadeController.cs
@@ -24,10 +24,56 @@ namespace Cutting_edge_webapp.Controllers
         }
 
         [HttpPost]
-        public ActionResult Index(DropCascade formdata)
+        public ActionResult Index(DropCascade formData)
         {
             //TODO
-            throw new NotImplementedException();
+            
+            return RedirectToAction("Index", "Home");
         }
+
+        //
+        // POST: /Country/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Country country)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(country);
+        }
+
+        //[HttpPost]
+        //public ActionResult Index(ApplicationForm formdata)
+        //{
+        //    if (formdata.Name == null)
+        //    {
+        //        ModelState.AddModelError("Name", "Name is required field.");
+        //    }
+        //    if (formdata.State == null)
+        //    {
+        //        ModelState.AddModelError("State", "State is required field.");
+        //    }
+        //    if (formdata.District == null)
+        //    {
+        //        ModelState.AddModelError("District", "District is required field.");
+        //    }
+
+        //    if (!ModelState.IsValid)
+        //    {
+        //        //Populate the list again
+        //        List<SelectListItem> state = new List<SelectListItem>();
+        //        state.Add(new SelectListItem { Text = "Bihar", Value = "Bihar" });
+        //        state.Add(new SelectListItem { Text = "Jharkhand", Value = "Jharkhand" });
+        //        ViewBag.StateName = new SelectList(state, "Value", "Text");
+
+        //        return View("Index");
+        //    }
+
+        //    //TODO: Database Insertion
+
+        //    return RedirectToAction("Index", "Home");
+        //}
     }
 }
